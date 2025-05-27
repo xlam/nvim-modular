f4df908a74b481f0f44b54ac3a1b5aad1f882c70
Sergey Sokolov <xlamserg@gmail.com>
2025-05-27 20:43:09 +0400

    Fine-tune a couple of keymaps
        * Add a comment to <C-w>= mapping.
        * Add floating neo-tree mapping.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -92,7 +92,7 @@ vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 -- Window management
 -- vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
 -- vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+vim.keymap.set('n', '<leader>se', '<C-w>=', { noremap = true, silent = true, desc = 'Make split windows equal width & height' }) -- make split windows equal width & height
 vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<Leader>e', ':Neotree float<CR>', desc = 'NeoTree float', silent = true },
   },
   opts = {
     filesystem = {
